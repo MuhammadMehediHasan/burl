eff6646d02ec40ec256e22a8aa620f4c5d96242b
MuhammadMehediHasan <mehedisohan1111@gmail.com>
2023-10-25 03:19:23 +0600

    Refactor + Clean Documantation

--- burl.js
+++ burl.js
@@ -1,9 +1,6 @@
-
-
 class BURLElement extends HTMLElement {
   constructor() {
     super();
-
 
     if (!this.hasAttribute("href"))
       throw new Error("href attribute value missing");
@@ -23,7 +20,7 @@ class BURLElement extends HTMLElement {
   </svg>`;
   }
 
-   connectedCallback() {
+  connectedCallback() {
     let urlHandler = new URL(this.#href());
 
     this.classList.add("burl");
@@ -41,14 +38,16 @@ class BURLElement extends HTMLElement {
     } else {
       span.textContent =
         urlHandler.pathname == "/" ? urlHandler.hostname : urlHandler.pathname;
+      // max-width: 22ch;
+      span.style.maxWidth = "22ch";
     }
 
     if (!this.hasAttribute("chip")) {
       // img
       if (!this.hasAttribute("no-icon")) {
         const img = document.createElement("img");
-        if(this.hasAttribute('icon')) {
-          img.src = this.getAttribute('icon');
+        if (this.hasAttribute("icon")) {
+          img.src = this.getAttribute("icon");
         } else {
           img.src = this.#getFaviconLink(this.#href());
         }
@@ -72,26 +71,25 @@ class BURLElement extends HTMLElement {
   }
 }
 
-/**
-  @name template
-
-<div class="link">
-<a href="google.com">
-  <img
-    src="https://www.google.com/s2/favicons?domain=google.com&sz=64"
-    alt="favicon"
-  />
-  <span>Google</span>
-  <div class="icon">
-    <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512">  
-      <path
-        d="M352 0c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9L370.7 96 201.4 265.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L416 141.3l41.4 41.4c9.2 9.2 22.9 11.9 34.9 6.9s19.8-16.6 19.8-29.6V32c0-17.7-14.3-32-32-32H352zM80 32C35.8 32 0 67.8 0 112V432c0 44.2 35.8 80 80 80H400c44.2 0 80-35.8 80-80V320c0-17.7-14.3-32-32-32s-32 14.3-32 32V432c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V112c0-8.8 7.2-16 16-16H192c17.7 0 32-14.3 32-32s-14.3-32-32-32H80z"
-      />
-    </svg>
-  </div>
-</a>
- </div>
-
-*/
-
 window.customElements.define("b-url", BURLElement);
+
+/**
+  @generated
+
+<burl href="<link>">
+   <a href="google.com">
+      <img
+         src="https://www.google.com/s2/favicons?domain=google.com&sz=64"
+         alt="favicon"
+         />
+      <span>Google</span>
+      <div class="icon">
+         <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512">
+            <path
+               d="M352 0c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9L370.7 96 201.4 265.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L416 141.3l41.4 41.4c9.2 9.2 22.9 11.9 34.9 6.9s19.8-16.6 19.8-29.6V32c0-17.7-14.3-32-32-32H352zM80 32C35.8 32 0 67.8 0 112V432c0 44.2 35.8 80 80 80H400c44.2 0 80-35.8 80-80V320c0-17.7-14.3-32-32-32s-32 14.3-32 32V432c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V112c0-8.8 7.2-16 16-16H192c17.7 0 32-14.3 32-32s-14.3-32-32-32H80z"
+               />
+         </svg>
+      </div>
+   </a>
+</burl>
+*/
